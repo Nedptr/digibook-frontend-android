cc0d3e5d4862f495f7e888a4cf00bbf96393502a
Nedptr <houssem.ferjaoui@esprit.tn>
2021-01-03 14:12:53 +0100

    before profile edit

--- app/src/main/java/com/example/digibook/fragments/SearchFragment.java
+++ app/src/main/java/com/example/digibook/fragments/SearchFragment.java
@@ -2,6 +2,7 @@ package com.example.digibook.fragments;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.CursorIndexOutOfBoundsException;
@@ -39,6 +40,12 @@ public class SearchFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private static final int READ_STORAGE_PERMISSION_REQUEST_CODE = 1 ;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
+    private static final int WRITE_STORAGE_PERMISSION_REQUEST_CODE = 1;
+    private static final int IMAGE_CAPTURE_CODE = 1;
+
+
+    private Uri image_uri;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -75,6 +82,7 @@ public class SearchFragment extends Fragment {
         }
 
 
+
     }
 
     @Override
@@ -83,6 +91,7 @@ public class SearchFragment extends Fragment {
 
         View viewroot = inflater.inflate(R.layout.fragment_search, container, false);
         Button uploadButton = (Button) viewroot.findViewById(R.id.UploadImage);
+        Button takeimage = (Button) viewroot.findViewById(R.id.TakeImage);
 
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,16 +112,42 @@ public class SearchFragment extends Fragment {
                 // PICK_IMAGE = 1
                 startActivityForResult(Intent.createChooser(gallery, "Select Image"), 1);
 
+
             }
         });
 
-        // Inflate the layout for this fragment
+        takeimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(checkPermissionForReadExtertalStorage() == false) {
+                    try {
+                        requestPermissionForReadExtertalStorage();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                values.put(MediaStore.Images.Media.DESCRIPTION,"From the Cam");
+                image_uri = viewroot.getContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                // Cam intent
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+                startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+
+            }
+        });
+
         return viewroot;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        //Log.d("FILEPATHBRO", "File uri: " + data.getData().toString());
 
         if(requestCode == 1 && resultCode == RESULT_OK){
             Uri uri = data.getData();
@@ -132,6 +167,24 @@ public class SearchFragment extends Fragment {
             CurrentSession.uploadImageSearch(path, getContext());
         }
 
+/*        if(requestCode == IMAGE_CAPTURE_CODE && resultCode == RESULT_OK){
+            Uri uri = image_uri;
+            String path = null;
+            if (Build.VERSION.SDK_INT < 11)
+                path = RealPathUtils.getRealPathFromURI_BelowAPI11(getContext(), uri);
+
+                // SDK >= 11 && SDK < 19
+            else if (Build.VERSION.SDK_INT < 19)
+                path = RealPathUtils.getRealPathFromURI_API11to18(getContext(), uri);
+
+                // SDK > 19 (Android 4.4)
+            else
+                path = RealPathUtils.getRealPathFromURI_API19(getContext(), uri);
+            Log.d("FILEPATHBRO", "File Path: " + path);
+            // Get the file instance
+            CurrentSession.uploadImageSearch(path, getContext());
+        }*/
+                //  content://com.android.providers.media.documents/document/image%3A36754
     }
 
     public boolean checkPermissionForReadExtertalStorage() {
@@ -144,11 +197,48 @@ public class SearchFragment extends Fragment {
 
     public void requestPermissionForReadExtertalStorage() throws Exception {
         try {
-            ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+            ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
                     READ_STORAGE_PERMISSION_REQUEST_CODE);
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
     }
+
+/*    //
+    public boolean checkPermissionForWriteExtertalStorage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int result = getContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            return result == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    public void requestPermissionForWriteExtertalStorage() throws Exception {
+        try {
+            ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_STORAGE_PERMISSION_REQUEST_CODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+    
+    public boolean checkPermissionForCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int result = getContext().checkSelfPermission(Manifest.permission.CAMERA);
+            return result == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    public void requestPermissionForCamera() throws Exception {
+        try {
+            ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSION_REQUEST_CODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }*/
 }
